d43382c5b641c68006cf7fecc32d7e7b0cd1b444
brcodington78 <brcodington@gmail.com>
2021-06-14 11:23:37 -0700

    making component skeletons

---
+++ frontend/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Yeti's List</h1>
+        <footer>
+          Copyright &copy; 2021 Arctech
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
