8409e611b6b8eda19c438452c002f5eb3f03aeb2
brcodington78 <brcodington@gmail.com>
2021-06-15 12:14:08 -0700

    modal sessioForm file made and filled out

---
+++ frontend/src/components/session/session_form.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+
+class SessionForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            password: ''
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    update(field) {
+        return e => this.handleSubmit.setState({
+            [field]: e.currentTarget.value
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        const user = Object.assign({}, this.state);
+        this.props.processForm(user).then(this.props.closeModal)
+    }
+
+    renderErrors() {
+        return(
+            <ul>
+                {this.props.errors.map((error, i) => (
+                    <li key={`error-${i}`}>
+                        {error}
+                    </li>
+                ))}
+            </ul>
+        );
+    }
+
+    render() {
+        let username_input;
+        if (this.props.formType === 'signup') {
+            username_input = ( 
+                <label>Username:
+                    <input type="text" 
+                        value={this.state.username}
+                        onChange={this.update('username')}
+                        className='signup-input'/>
+                </label>
+            )
+        }
+        return (
+            <div className="login-form-container">
+                <form onSubmit={this.handleSubmit} className="login-form-box">
+                    Welcome to Yeti's List!
+                
+                Please {this.props.formType} or {this.props.otherForm}
+                    <div onClick={this.props.closeModal} className="close-x">
+                        X
+                    </div>
+                    {this.renderErrors()}
+                    <div className ='login-form'>
+                       <br/> 
+                        {username_input}
+                        <label>Email:
+                            <input type='text'
+                                value={this.state.email}
+                                onChange={this.update('email')}
+                                className="login-input"
+                            />
+                        </label>
+                        <br/>
+                        <label>Password:
+                            <input type="password"
+                                value={this.state.password}
+                                onChange={this.update('password')}
+                                className="login-input"
+                                />
+                        </label>
+                        <br/>
+                        <input className='session-submit' type='submit' value={this.props.formType} />
+                    </div>
+                </form>
+            </div>
+        );
+    }
+
+}
+
+export default withRouter(SessionForm);
