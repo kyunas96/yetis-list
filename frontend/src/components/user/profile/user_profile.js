ec7eab2ccc023d06ec08ae4192c22d368bedfe19
Schploink <thekpoc@gmail.com>
2021-06-16 14:21:50 -0700

    update branch to switch work focus

--- frontend/src/components/user/profile/user_profile.js
+++ frontend/src/components/user/profile/user_profile.js
@@ -6,7 +6,15 @@ class UserProfilePage extends React.Component {
 			<div>
 				<h1 className="main-title">Yeti's List</h1>
 				<h1 className="user-greeting">{this.props.currentUser.username} Yeti friend!</h1>
-				{/* {this.props.logout} */}
+				<div className='your-playlist-box'>
+					Your playlists listed here
+				</div>
+				<div className='liked-playlist-box'>
+					Your liked playlists listed here
+				</div>
+				<div className="average-song-stats">
+					Average song stats from playlists here
+				</div>
 			</div>
 		);
 	}
