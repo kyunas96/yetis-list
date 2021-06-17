fa3e654dd835c773d30895a3705184d8e405daaa
Schploink <thekpoc@gmail.com>
2021-06-17 11:45:38 -0700

    main page styling

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { openModal } from '../../actions/modal_actions'
 import {connect} from 'react-redux'
+import Yeti from '../svg/yeti-component'
 import './main.css'
 
 class MainPage extends React.Component {
 
   render() {
     return (
-      <div>
+      <div className="main-body">
         <h1 className="main-title">Yeti's List</h1>
           <div className="site-info">Yeti help friend make music playlist. Please sign up and tell Yeti song/album/genre you like, and Yeti help friend!</div>
           <div className="buttons">
             {this.props.openSignup}
             {this.props.openLogin}
           </div>
+          <Yeti />
         <footer className="footer">
           Copyright &copy; 2021 YeticorpLLC
         </footer>
---
+++ frontend/src/components/svg/yeti-component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Yeti from './Yeti2.js';
+import './yeti.css'
+
+const yeti = () => {
+  return (
+    <div className="yeti">
+      <Yeti />
+    </div>
+  );
+}
+export default yeti;
