33b963b5fb2f737acae56e4bc17e2502b71d8697
brcodington78 <brcodington@gmail.com>
2021-06-18 13:49:40 -0700

    added artist names to tracks for search bar in list_item file

--- frontend/src/components/search/list.js
+++ frontend/src/components/search/list.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ListItem from './list_item';
 
 const List = props => {
-  console.log(props)
+  console.log(props, 'List logger')
   return props.items.length > 0 ? (
   <ul className='list'>
     {props.items.map(item => (
--- frontend/src/components/search/list_item.js
+++ frontend/src/components/search/list_item.js
@@ -3,16 +3,58 @@ import './search.css'
 
 const ListItem = (props) => {
   let imageSrc = props.item.image.small ? props.item.image.small : "music-solid.svg";
+  
+  console.log(props.item, 'items')
 
-  console.log(props);
+
+  const getArtists = () => {
+    if (props.item.artist !== undefined) {
+      let artistsStr = props.item.artist.join(', ')
+      if (artistsStr.length > 20) {
+        artistsStr = artistsStr.slice(0,18) + '...'
+        console.log(artistsStr)
+      }
+      let trackArtistText;
+      if (artistsStr === undefined) {
+        trackArtistText = null
+      } else {
+        trackArtistText = <span className='track-artists-text'>{artistsStr}</span>
+      }
+      return trackArtistText
+      
+    } else {
+      return null
+    }
+
+    
+  }
+  
+  const nameShortener = () => {
+    let displayName = '';
+    if (props.item.name.length > 40) {
+      displayName = props.item.name.slice(0,37) + '...'
+      return displayName
+    } else {
+      return props.item.name
+    }
+   
+
+  }
+  
+
+
+
   return (
     <li
       className="list-item"
       onClick={() => props.action(props.item.id, props.item.name)}
       key={props.id}
-    >
+    > 
       <img className="album-art-search" src={imageSrc}></img>
-      <span className="search-value-name">{props.item.name}</span>
+      <div className='info-text'>
+        <span className="search-value-name">{nameShortener()}</span>
+        {getArtists()}
+      </div>
     </li>
   );
 };
