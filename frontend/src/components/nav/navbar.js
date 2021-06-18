bdd742b930adb4d4ce1a39937c6ad5c86d24246a
Elijah Ally <elijahally18@gmail.com>
2021-06-18 10:29:49 -0400

    fixed nav bar css and update playlist modal bug

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,58 +1,65 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import './navbar.css'
-
+import { Link } from 'react-router-dom';
+import './navbar.css';
 
 class NavBar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getLinks = this.getLinks.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.getLinks = this.getLinks.bind(this);
+	}
 
-  // Selectively render links dependent on whether the user is logged in
+	// Selectively render links dependent on whether the user is logged in
 
-  shouldComponentUpdate(nextProps) {
-    if (nextProps !== this.props) {
-      return true;
-    } else {
-      return false;
-    }
-  }
+	shouldComponentUpdate(nextProps) {
+		if (nextProps !== this.props) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-
-  getLinks() {
-      if (this.props.loggedIn) {
-        return (
-            <ul className="logged-in-nav">
-              <li>
-                <p onClick={() => this.props.openModal()} className="playlist-make-nav">Make a custom playlist</p> 
-              </li>
-              <li>
-                <Link to={`/users/${this.props.user}/profile`} className="profile-nav-link">Profile</Link>
-              </li>
-              <li>
-                <button className="logout-button" onClick={() => this.props.logout()}>Logout</button>
-              </li>
-            </ul>
-        );
-      } else {
-        return (
-            <ul>
-            </ul>
-        );
-      }
-  }
-
-  render() {
-      return (
-        <div className="navbar-container">
-            <div className="nav-title">
+	getLinks() {
+		if (this.props.loggedIn) {
+			return (
+				<>
+          <div className='nav-title'>
+            <Link to={`/users/${this.props.user}`}>
               Yeti's List
-            </div>
-            { this.getLinks() }
-        </div>
-      );
-  }
+            </Link>
+          </div>
+					<ul className='logged-in-nav'>
+						<li>
+							<p
+								onClick={() => this.props.openModal()}
+								className='playlist-make-nav'>
+								Make a custom playlist
+							</p>
+						</li>
+						<li>
+							<Link
+								to={`/users/${this.props.user}/profile`}
+								className='profile-nav-link'>
+								Profile
+							</Link>
+						</li>
+						<li>
+							<button
+								className='logout-button'
+								onClick={() => this.props.logout()}>
+								Logout
+							</button>
+						</li>
+					</ul>
+				</>
+			);
+		} else {
+			return <div className='nav-title'>Yeti's List</div>;
+		}
+	}
+
+	render() {
+		return <div className='navbar-container'>{this.getLinks()}</div>;
+	}
 }
 
 export default NavBar;
