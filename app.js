23a900df090f21a280909a5239b8860b74c4fdeb
Elijah Ally <elijahally18@gmail.com>
2021-06-28 20:17:49 -0400

    added console.log to app

--- app.js
+++ app.js
@@ -21,7 +21,10 @@ if (process.env.NODE_ENV === 'production') {
 mongoose
 	.connect(db, { useNewUrlParser: true , useUnifiedTopology: true })
 	.then(() => console.log('Connected to MongoDB successfully'))
-	.catch((err) => console.log(err));
+	.catch((err) => {
+		console.log('not connected to MongoDB')
+		console.log(err)
+	});
 
 app.use(passport.initialize());
 require('./config/passport')(passport);
