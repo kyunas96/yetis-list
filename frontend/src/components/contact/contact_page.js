57bbfdb2677024b1beadb21b6bcbd68693dbe8e7
brcodington78 <brcodington@gmail.com>
2021-06-29 09:40:23 -0700

    edits to dev_box

--- frontend/src/components/contact/contact_page.js
+++ frontend/src/components/contact/contact_page.js
@@ -8,10 +8,10 @@ function ContactPage(props) {
 
     return (
         <div className='contact-page-container'>
-            <DevBox name='Kevin Yunas' gitLink='https://github.com/yunasty' profile-pic='https://avatars.githubusercontent.com/u/22901798?v=4' linkedin='templinkforplacement'/>
-            <DevBox name="Kevin O'Connor" gitLink='https://github.com/Schploink' profile-pic='https://avatars.githubusercontent.com/u/77660759?v=4' linkedin='templinkforplacement'/>
-            <DevBox name='Elijah Ally' gitLink='https://github.com/ElijahAlly' profile-pic='https://avatars.githubusercontent.com/u/75961076?v=4' linkedin='templinkforplacement'/>
-            <DevBox name='Brian Codington' gitLink='https://github.com/brcodington78' profile-pic='https://avatars.githubusercontent.com/u/80482919?v=4' linkedin='templinkforplacement'/>
+            <DevBox name='Kevin Yunas' gitLink='https://github.com/yunasty' profilePic='https://avatars.githubusercontent.com/u/22901798?v=4' linkedin='templinkforplacement' linkedin='placeholder'/>
+            <DevBox name="Kevin O'Connor" gitLink='https://github.com/Schploink' profilePic='https://avatars.githubusercontent.com/u/77660759?v=4' linkedin='templinkforplacement' linkedin='placeholder'/>
+            <DevBox name='Elijah Ally' gitLink='https://github.com/ElijahAlly' profilePic='https://avatars.githubusercontent.com/u/75961076?v=4' linkedin='templinkforplacement' linkedin='placeholder'/>
+            <DevBox name='Brian Codington' gitLink='https://github.com/brcodington78' profilePic='https://avatars.githubusercontent.com/u/80482919?v=4' linkedin='templinkforplacement' linkedin='placeholder'/>
         </div>
     )
 }
