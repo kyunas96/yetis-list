484f19c8fff93fe623cead8097dc251e20b590a5
brcodington78 <brcodington@gmail.com>
2021-06-28 20:59:22 -0700

    making devbox component

--- frontend/src/components/contact/contact_page.js
+++ frontend/src/components/contact/contact_page.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import {connect} from 'react-redux'
+import DevBox from './dev_box';
 
 
-function contactPage(props) {
+function ContactPage(props) {
 
 
 
     return (
         <div className='contact-page-container'>
-            <DevBox name='Kevin Yunas' gitLink='https://github.com/yunasty' profile-pic='https://avatars.githubusercontent.com/u/22901798?v=4'/>
-            <DevBox name="Kevin O'Connor" gitLink='https://github.com/Schploink' profile-pic='https://avatars.githubusercontent.com/u/77660759?v=4'/>
-            <DevBox name='Elijah Ally' gitLink='https://github.com/ElijahAlly' profile-pic='https://avatars.githubusercontent.com/u/75961076?v=4'/>
-            <DevBox name='Brian Codington' gitLink='https://github.com/brcodington78' profile-pic='https://avatars.githubusercontent.com/u/80482919?v=4'/>
+            <DevBox name='Kevin Yunas' gitLink='https://github.com/yunasty' profile-pic='https://avatars.githubusercontent.com/u/22901798?v=4' linkedin='templinkforplacement'/>
+            <DevBox name="Kevin O'Connor" gitLink='https://github.com/Schploink' profile-pic='https://avatars.githubusercontent.com/u/77660759?v=4' linkedin='templinkforplacement'/>
+            <DevBox name='Elijah Ally' gitLink='https://github.com/ElijahAlly' profile-pic='https://avatars.githubusercontent.com/u/75961076?v=4' linkedin='templinkforplacement'/>
+            <DevBox name='Brian Codington' gitLink='https://github.com/brcodington78' profile-pic='https://avatars.githubusercontent.com/u/80482919?v=4' linkedin='templinkforplacement'/>
         </div>
     )
 }
+
+export default ContactPage
 
 
 
---
+++ frontend/src/components/contact/dev_box.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './contact.css'
+
+
+
+function DevBox(props) {
+
+    return (
+        <div className='dev-container'>
+            
+        </div>
+    )
+}
+
+export default DevBox
