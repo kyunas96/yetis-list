394ef552433e1840e5b54a74f08c3e606827e49f
Elijah Ally <elijahally18@gmail.com>
2021-06-30 17:51:10 -0400

    fixed add comment button

--- frontend/src/components/comment/comments_list.js
+++ frontend/src/components/comment/comments_list.js
@@ -13,17 +13,17 @@ const CommentList = ({comments, openModal }) => {
 
     return ( 
         <section className='comments'>
+            <div className='comment-item add-comment' onClick={() => openModal()}>Click To Add A Comment</div>
             <ul className='comments-list'>
                 {(comments && comments.length > 0)? (
                     <>
-                        <li className='comment-item add-comment' onClick={() => openModal()}>Click To Add A Comment</li>
                         {classComments.map((comment, i) => {
                             console.log(comment)
                             return <CommentItem key={i} comment={comment}/>
                         })}
                     </>
                 ) : (
-                    <li className='comment-item add-comment' onClick={() => openModal()}>Click To Add A Comment</li>
+                    <></>
                 )}
             </ul>
         </section>
