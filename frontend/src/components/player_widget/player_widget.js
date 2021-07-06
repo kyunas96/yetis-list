d0e1923f8a839143a1ffb731cffda71dd5765b0b
Elijah Ally <elijahally18@gmail.com>
2021-07-06 18:10:17 -0400

    styled widget and spotify button

--- frontend/src/components/player_widget/player_widget.js
+++ frontend/src/components/player_widget/player_widget.js
@@ -1,33 +1,23 @@
-import {connect} from 'react-redux'
-import React from 'react'
-import {withRouter} from "react-router"
-import './widget.css'
+import { connect } from 'react-redux';
+import './widget.css';
 
-class PlayerWidget extends React.Component {
-  constructor(props) {
-    super(props)
-  }
-
-
-  render() {
-    return (
-      <>
-      <div className="widget-player-container">
-        <iframe className="widget-player" src={`https://open.spotify.com/embed/track/${this.props.songId}?theme=0`} frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-      </div>
-      <img className="widget-toggle" src="https://image.flaticon.com/icons/png/512/808/808531.png"/>
-      </>
-    )
-  }
-}
+const PlayerWidget = ({ songId }) => {
+	return (
+		<div className='widget-player-container'>
+			<iframe
+				className='widget-player'
+				src={`https://open.spotify.com/embed/track/${songId}?theme=0`}
+				frameBorder='0'
+				allowtransparency='true'
+				allow='encrypted-media'></iframe>
+		</div>
+	);
+};
 
 const mapStateToProps = (state, ownProps) => {
-  return {
-    songId: state.ui.widget
-  };
+	return {
+		songId: state.ui.widget,
+	};
 };
-  
-export default withRouter(
-  connect(mapStateToProps)(PlayerWidget)
-);
-  
+
+export default connect(mapStateToProps)(PlayerWidget);
