b01b2247008c9ad8b42e59fadc877bad3c0c3d6a
Kevin Yunas <kevinyunas@icloud.com>
2021-07-08 14:49:48 -0700

    Cleaned up util files

--- frontend/src/util/comment_api_util.js
+++ frontend/src/util/comment_api_util.js
@@ -1,13 +1,5 @@
 import axios from 'axios';
 
-export const setAuthToken = (token) => {
-	if (token) {
-		axios.defaults.headers.common['Authorization'] = token;
-	} else {
-		delete axios.defaults.headers.common['Authorization'];
-	}
-};
-
 export const createComment = (comment) => {
 	return axios.post(`/api/comments/`, comment);
 };
--- frontend/src/util/song_api_util.js
+++ frontend/src/util/song_api_util.js
@@ -1,12 +1,4 @@
 import axios from 'axios';
-
-export const setAuthToken = token => {
-  if (token) {
-    axios.defaults.headers.common['Authorization'] = token;
-  } else {
-    delete axios.defaults.headers.common['Authorization'];
-  }
-};
  
 export const addSongToPlaylist = (song) => {
   return axios.post(`/api/songs/`, song);
