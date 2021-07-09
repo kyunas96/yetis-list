69d1366a792980b99c45a0c855a5b47d3518be12
Kevin Yunas <kevinyunas@icloud.com>
2021-07-08 18:01:18 -0700

    Fixed alignment on playlist feed page

--- frontend/src/components/playlist/playlist_feed_list.js
+++ frontend/src/components/playlist/playlist_feed_list.js
@@ -1,59 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const PlaylistFeedList = ({playlists, currentUserId, sendPlaylistId}) => {
+const PlaylistFeedList = ({ playlists, currentUserId, sendPlaylistId }) => {
+  const formatTitleDescriptionUsername = (type, info) => {
+    if (type === "title" && info.length > 30) {
+      info = info.slice(0, 27) + "...";
+    }
 
-    const formatTitleDescriptionUsername = (type, info) => {
-		if (type === 'title' && info.length > 30) {
-			info = info.slice(0, 27) + '...';
-		}
-        
-		if (type === 'uername' && info.length > 20) {
-			info = info.slice(0, 17) + '...';
-		}
-		
-        if (type === 'description' && info.length > 44) {
-			info = info.slice(0, 41) + '...';
-		}
+    if (type === "uername" && info.length > 20) {
+      info = info.slice(0, 17) + "...";
+    }
 
-		return info;
-	}
+    if (type === "description" && info.length > 44) {
+      info = info.slice(0, 41) + "...";
+    }
 
-    return (
-        <ul className='playlist-list-ul'>
-            {playlists.length > 0 ? (
-                playlists.map((playlist, i) => {
-                    return (
-                        <li
-                            key={i}
-                            className='feed-item-li'
-                            onClick={() => sendPlaylistId(playlist._id)}>
-                            <Link
-                                className="feed-item"
-                                to={`/users/${currentUserId}/playlist/${playlist._id}`}>
-                                <h3 className='title-feed'>
-                                    {formatTitleDescriptionUsername('title', playlist.title)}
-                                </h3>
-                                <h3 className='username-feed'>
-                                    {formatTitleDescriptionUsername('username', playlist.username)}
-                                </h3>
-                                <h3 className='description-feed'>
-                                    {formatTitleDescriptionUsername(
-                                        'description',
-                                        playlist.description
-                                    )}
-                                </h3>
-                            </Link>
-                        </li>
-                    );
-                })
-            ) : (
-                <li>
-                    <h3>No Body Has Any Playlists</h3>
-                </li>
-            )}
-        </ul>
-    );
-}
- 
+    return info;
+  };
+
+  return (
+    <ul className="playlist-list-ul">
+      {playlists.length > 0 ? (
+        playlists.map((playlist, i) => {
+          return (
+            <li
+              key={i}
+              className="feed-item-li"
+              onClick={() => sendPlaylistId(playlist._id)}
+            >
+              <Link
+                className="feed-item"
+                to={`/users/${currentUserId}/playlist/${playlist._id}`}
+              >
+                <h3 className="feed-item-title">
+                  {formatTitleDescriptionUsername("title", playlist.title)}
+                </h3>
+                <div className="feed-item-title">
+                  <h3 id="username">
+                    {formatTitleDescriptionUsername(
+                      "username",
+                      playlist.username
+                    )}
+                  </h3>
+                </div>
+                <h3 className="feed-item-title">
+                  {formatTitleDescriptionUsername(
+                    "description",
+                    playlist.description
+                  )}
+                </h3>
+              </Link>
+            </li>
+          );
+        })
+      ) : (
+        <li>
+          <h3>No Body Has Any Playlists</h3>
+        </li>
+      )}
+    </ul>
+  );
+};
+
 export default PlaylistFeedList;
